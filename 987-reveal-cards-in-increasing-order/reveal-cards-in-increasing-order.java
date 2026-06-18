190e4eb3ca3889cf4dcdec3458f2b347183e41f4
Priyanshu Shekhar <priyanshushekhar019@gmail.com>
2026-06-19 02:03:57 +0530

    Time: 7 ms (94.30%) | Memory: 46 MB (14.79%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int n=deck.length;
+        int[] ans =new int[n];
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            q.offer(i);
+        }
+        for(int card:deck){
+            int idx=q.poll();
+            ans[idx]=card;
+            if(!q.isEmpty()){
+                q.offer(q.poll());
+            }
+        }
+        return ans;
+    }
+}
